d1dc1f4ea6ed13bceee0354888bf9721d4aa371a
PetoPettinari <petopettinari@gmail.com>
2023-06-15 00:27:37 -0300

    crud

--- apps/producto/producto/forms.py
+++ apps/producto/producto/forms.py
@@ -7,3 +7,18 @@ class ProductoCategoriaForm(forms.ModelForm):
     class Meta:
         model = models.ProductoCategoria
         fields = "__all__"
+        
+        widgets = {
+            "nombre": forms.TextInput(attrs={"class":"form-control"}),
+            "descripcion": forms.Textarea(attrs={"class":"form-control"}),
+        }
+
+class ProductoForm(forms.ModelForm):
+    class Meta:
+        model = models.Producto
+        fields = "__all__"
+        
+        widgets = {
+            "nombre": forms.TextInput(attrs={"class":"form-control"}),
+            "descripcion": forms.Textarea(attrs={"class":"form-control"}),
+        }
